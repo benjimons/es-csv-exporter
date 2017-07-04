dd25fc940050d9f87bbb44a97e137f1ebb9df898
Ben McDowall <ben.mcdowall@gmail.com>
2017-07-05 07:46:28 +1200

    Update inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -69,7 +69,7 @@ function parseTable(){
     //Convert the table tags to commas and white spaces
     h = h.replace(/<\/tr>/g, "\n");
     h = h.replace(/<\/dd>/g, ",");
-    h = h.replace(/<\/th>/g, ",");
+    h = h.replace(/<\/td>/g, ",");
     h = h.replace(/<\/th>/g, ",");
     h = h.replace(/( )?<.+?>( )?/g, "");
 
